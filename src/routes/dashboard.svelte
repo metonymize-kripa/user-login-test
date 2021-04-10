49afcccd29cc52be03b7d302f82ace07cb8025ca
metonymize-kripa <kripa@metonymize.com>
2021-04-10 16:20:58 -0500

    Added content and firebase authentication info

---
+++ src/routes/dashboard.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+	export async function preload(page, session) {
+		let { user } = session;
+		if (!user) {
+		    return this.redirect(302, '/login');
+		}
+	}
+</script>
+
+
+<h1>This is our protected dashboard! Only visible when you are logged in with Firebase</h1>
+
---
+++ src/routes/login.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { goto } from '@sapper/app';
+    
+    let email = '';
+    let password = '';
+
+    async function login() {
+        try {
+            firebase.auth().signInWithEmailAndPassword(email, password).then((res) => {
+                goto('/dashboard');
+            });
+        } catch(e) {
+            let message = e.message || e;
+            console.log("Something went wrong:", message);
+        }
+    }
+</script>
+
+<h1>Login form</h1>
+
+<input type="email" bind:value={email} placeholder="your email"/>
+<br />
+
+<input type="password" bind:value={password} placeholder="your password"/>
+<br />
+
+<button on:click={login}>Login</button>

cd9ad6118c928228abef8eadcf709c126ef2191d
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-13 06:45:09 -0500

    test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,51 +1,59 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+ 
+path {{
+  fill: yellow;
+  stroke: #000;
+}}
+ 
+circle {{
+  fill: #fff;
+  stroke: #000;
+  pointer-events: none;
+}}
+ 
+.PiYG .q0-9{{fill:rgb{colorArray[0]}}}
+.PiYG .q1-9{{fill:rgb{colorArray[1]}}}
+.PiYG .q2-9{{fill:rgb{colorArray[2]}}}
+.PiYG .q3-9{{fill:rgb{colorArray[3]}}}
+.PiYG .q4-9{{fill:rgb{colorArray[4]}}}
+.PiYG .q5-9{{fill:rgb{colorArray[5]}}}
+.PiYG .q6-9{{fill:rgb{colorArray[6]}}}
+.PiYG .q7-9{{fill:rgb{colorArray[7]}}}
+.PiYG .q8-9{{fill:rgb{colorArray[7]}}}
+ 
 </style>
-
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>SVG inline below</h1>
-
-<svg width="100" height="100">
-   <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
-   Sorry, your browser does not support inline SVG.
-</svg>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<body>
+<script src="https://d3js.org/d3.v3.min.js"></script>
+<script>
+ 
+width = 960, height = 500;
+ 
+vertices = d3.range(100).map(function(d) {{
+  return [Math.random() * width, Math.random() * height];
+}});
+ 
+svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "PiYG")
+    .on("mousemove", function() {{ vertices[0] = d3.mouse(this); redraw(); }});
+ 
+path = svg.append("g").selectAll("path");
+ 
+svg.selectAll("circle")
+    .data(vertices.slice(1))
+  .enter().append("circle")
+    .attr("transform", function(d) {{ return "translate(" + d + ")"; }})
+    .attr("r", 2);
+ 
+redraw();
+ 
+function redraw() {{
+  path = path.data(d3.geom.delaunay(vertices).map(function(d) {{ return "M" + d.join("L") + "Z"; }}), String);
+  path.exit().remove();
+  path.enter().append("path").attr("class", function(d, i) {{ return "q" + (i % 9) + "-9"; }}).attr("d", String);
+}}
+ 
+</script>

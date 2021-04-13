9cbe627e046baa7cdf0ca02964aeaf1cebd9748f
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-13 06:47:25 -0500

    test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -42,7 +42,3 @@
    <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
    Sorry, your browser does not support inline SVG.
 </svg>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>

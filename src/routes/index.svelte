0378dcf1473ea2c288ab7e8e5aab680b21c97701
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-13 06:41:34 -0500

    test svg

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -36,7 +36,12 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>SVG inline below</h1>
+
+<svg width="100" height="100">
+   <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+   Sorry, your browser does not support inline SVG.
+</svg>
 
 <figure>
 	<img alt='Borat' src='great-success.png'>
